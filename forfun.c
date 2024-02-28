c0c675640bd456bf657fba4a99054218a73a7acf
Jonathan Shields <jshieldsf@gmail.com>
2024-02-28 04:24:57 -0500

    Initialize repository

---
+++ forfun.c
@@ -0,0 +1,23 @@
+/* 
+ * Plan to execute:
+ *
+ * 1) Automate populating text file with random integers
+ * 2) Feed from populated text file to program
+ * 3) Take values and move to array
+ * 4) Use nested for loop to solve problem
+ * 5) Output results to output file
+ *
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	FILE* fopen("
+	
+	
+}
